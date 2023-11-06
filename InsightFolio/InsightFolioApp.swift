9e50adb0894632d99b9f1d2ada6e0aebc5b69060
Sarath Sasikala <sarath.vs@waracle.com>
2023-11-06 21:11:31 +0000

    Initial Commit

---
+++ InsightFolio/InsightFolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  InsightFolioApp.swift
+//  InsightFolio
+//
+//  Created by Sarath Sasikala on 06/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct InsightFolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
